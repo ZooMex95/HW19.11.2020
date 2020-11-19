895f9bc779904f2b1fb3537090809f6b3a15c70d
Evgenii Tolmachev <zoomex95@gmail.com>
2020-11-19 23:42:45 +0300

    Добавил интерфейс и переопределил методы

---
+++ src/Eatable.java
@@ -0,0 +1,8 @@
+public interface Eatable {
+    void add();
+    void del();
+    void del(int index);
+    int getWeight();
+    int getPrice();
+    void getDescription();
+}
---
+++ src/SweetBox.java
@@ -0,0 +1,31 @@
+public class SweetBox implements Eatable {
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void del() {
+
+    }
+
+    @Override
+    public void del(int index) {
+
+    }
+
+    @Override
+    public int getWeight() {
+        return 0;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+
+    @Override
+    public void getDescription() {
+
+    }
+}
